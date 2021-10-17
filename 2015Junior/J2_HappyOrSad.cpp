03e601a1f4f7c9ee11f476741c057f0194b0b43a
danh295 <hudanny295@gmail.com>
2021-10-17 11:11:36 -0400

    Added J2_HappyOrSad.cpp
    program uses a function (unnecessary but it's there)

---
+++ 2015Junior/J2_HappyOrSad.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countHappy = 0;
+int countSad = 0;
+
+string happy_or_sad(string sent)
+{
+    for(int i = 0; i < sent.size(); i++)
+    {
+        char curr = sent[i];
+        if(curr == ':')
+        {
+            if(sent[i+1] == '-')
+            {
+                if(sent[i+2] == '(')
+                    countSad += 1;
+                else if(sent[i+2] == ')')
+                    countHappy += 1;
+            }
+        }
+    }
+
+    if(countSad + countHappy == 0)
+        return "none";
+    else if(countSad == countHappy)
+        return "unsure";
+    else if(countSad > countHappy)
+        return "sad";
+    else
+        return "happy"; 
+}
+
+int main()
+{
+    string in;
+    getline(cin, in);
+    string out = happy_or_sad(in);
+    cout<<out;
+
+    return 0;
+}
