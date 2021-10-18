9b02676e7fd66d27cafea6b59530afc6eb771209
danh295 <hudanny295@gmail.com>
2021-10-18 13:06:31 -0400

    Updated file organization, added J1_TournamentSlection.cpp

---
+++ 2016/Junior/J1_TournamentSelection.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int groupPlayer(int wins)
+{
+    if(wins >= 5)
+        return 1;
+    else if(wins >= 3)
+        return 2;
+    else if(wins >= 1)
+        return 3;
+    else
+        return -1;
+}
+
+int main()
+{
+    char game;
+    int wins = 0;
+    for(int i = 0; i < 6; i++)
+    {
+        cin>>game;
+        if(game == 'W')
+            wins += 1;
+    }
+
+    cout<<groupPlayer(wins);
+    
+    return 0;
+}
