72f36ebfc73df0de4166a74481d8103343882d2a
danh295 <hudanny295@gmail.com>
2021-10-20 11:15:23 -0400

    Added 2017 folders, added J1_QuadrantSelectionc.pp

---
+++ 2017/Junior/J1_QuadrantSelection.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+
+    cin>>x;
+    cin>>y;
+
+    if(x > 0 && y > 0)
+        cout<<1;
+    else if(x > 0 && y < 0)
+        cout<<4;
+    else if(x <0 && y > 0)
+        cout<<2;
+    else
+        cout<<3;
+
+    return 0;
+}
