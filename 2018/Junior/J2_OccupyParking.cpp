f55c37ca20c18f48a758755dbf16cc489cddcbbe
danh295 <hudanny295@gmail.com>
2021-10-28 09:53:23 -0400

    Update J2_OccupyParking.cpp
    Added basic comments and program description

--- 2018/Junior/J2_OccupyParking.cpp
+++ 2018/Junior/J2_OccupyParking.cpp
@@ -1,3 +1,13 @@
+/*
+PROBLEM DESCRIPTION:
+You supervise a small parking lot which has 'N' parking spaces.
+
+Yesterday, you recorded which parking spaces were occupied by cars and which were empty.
+
+Today, you recorded the same information.
+
+How many of the parking spaces were occupied both yesterday and today?
+*/
 #include <iostream>
 #include <string>
 
@@ -5,8 +15,10 @@ using namespace std;
 
 int countOccupied(int avail, string yday, string tday)
 {
+    // process
     int occupied = 0;
     int i = 0;
+    
     while(i < avail)
     {
         if(yday[i] == 'C' && tday[i] == 'C')
@@ -19,13 +31,17 @@ int countOccupied(int avail, string yday, string tday)
 
 int main()
 {
+    // init
     int n;
     string ystday;
     string tday;
+
+    // input
     cin>>n;
     cin>>ystday;
     cin>>tday;
 
+    // output
     cout<<countOccupied(n, ystday, tday);
 
     return 0;
