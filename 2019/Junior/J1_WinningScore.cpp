e2f24d1528f32d9cfa111843375ec4eafa756562
danh295 <hudanny295@gmail.com>
2021-10-29 19:06:22 -0400

    Update J1_WinningScore.cpp

--- 2019/Junior/J1_WinningScore.cpp
+++ 2019/Junior/J1_WinningScore.cpp
@@ -10,45 +10,43 @@ the game ended in a tie.
 
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
     int aPts = 0;
     int bPts = 0;
     int temp;
 
-    cin>>temp;
+    std::cin>>temp;
     aPts += temp*3;
     temp = 0;
 
-    cin>>temp;
+    std::cin>>temp;
     aPts += temp*2;
     temp = 0;
 
-    cin>>temp;
+    std::cin>>temp;
     aPts += temp;
     temp = 0;
 
-    cin>>temp;
+    std::cin>>temp;
     bPts += temp*3;
     temp = 0;
 
-    cin>>temp;
+    std::cin>>temp;
     bPts += temp*2;
     temp = 0;
 
-    cin>>temp;
+    std::cin>>temp;
     bPts += temp*1;
     temp = 0;
 
-    cout<<aPts<<", "<<bPts<<endl;
+    std::cout<<aPts<<", "<<bPts<<std::endl;
     if(aPts > bPts)
-        cout<<'A';
+        std::cout<<'A';
     else if(bPts > aPts)
-        cout<<'B';
+        std::cout<<'B';
     else
-        cout<<'T';
+        std::cout<<'T';
 
     return 0;
 }
