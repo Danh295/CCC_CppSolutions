6b3509450d093ffb3e04742e13e6c27271d9a8c1
danh295 <hudanny295@gmail.com>
2021-10-29 18:43:11 -0400

    Added comments for J1_WinningScore.cpp

--- 2019/Junior/J1_WinningScore.cpp
+++ 2019/Junior/J1_WinningScore.cpp
@@ -1,3 +1,12 @@
+/*
+You record all of the scoring activity at a basketball game. Points are scored
+by a 3-point shot, a 2-piont field goal, or a 1-point free throw.
+
+You know the number of each of these types of scoring for teh two teams:
+the Apples and the Bananas. Your job is to determine which team won, or if
+the game ended in a tie.
+*/
+
 #include <iostream>
 
 using namespace std;
