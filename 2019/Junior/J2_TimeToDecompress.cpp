19d0856a4cd4787a40c011078d73f53728d62486
danh295 <hudanny295@gmail.com>
2021-11-02 23:37:43 -0400

    Minor commenting updates

--- 2019/Junior/J2_TimeToDecompress.cpp
+++ 2019/Junior/J2_TimeToDecompress.cpp
@@ -1,16 +1,31 @@
+/*
+You and your friend have come up with a way to send messages back and
+forth. 
+
+Your friend can encode a message to you by writing down a positive integer
+"N" and a symbol. You can encode that message by writing out that symbol "N"
+times on a row on one line.
+
+Give a message that your friend has encoded, decode it.
+*/
 #include <iostream>
 #include <string>
 
 int main()
 {
+    // INITIALIZATION + INPUT 
     int lines;
     std::cin>>lines;
+
+    // MAIN PROCESS + ITERATIVE INPUT
     for(int i = 0; i < lines; i++)
     {
         int n;
         char s;
         std::cin>>n;
         std::cin>>s;
+
+        // ITERATIVE OUTPUT
         for(int j = 0; j < n; j++)
         {
             std::cout<<s;
