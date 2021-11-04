6b641a038bde37ae8898d447a21ed42f5c5b6738
danh295 <hudanny295@gmail.com>
2021-11-04 10:22:19 -0400

    Minor comment updates

--- 2017/Junior/J2_ShiftySum.cpp
+++ 2017/Junior/J2_ShiftySum.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
+// MAIN PROCESS
 int shiftySum(int num, int shifts)
 {
     int sum = num;
-
     int s = 10;
+
+    // basic for-loop shifts value of outputing sum
     for(int i = 0; i < shifts; i++)
     {
         sum += num*s;
@@ -16,10 +18,14 @@ int shiftySum(int num, int shifts)
 
 int main()
 {
+    // INTITIALIZATION
     int n, k;
+
+    // INPUT
     std::cin>>n;
     std::cin>>k;
 
+    // OUTPUT
     std::cout<<shiftySum(n, k);
 
     return 0;
