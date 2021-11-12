76929d7d85e8905efcff5255611f8dbfb99e2a7b
danh295 <hudanny295@gmail.com>
2021-11-12 17:35:37 -0500

    Removed namespace std

--- 2012/Junior/J3_IconScaling.cpp
+++ 2012/Junior/J3_IconScaling.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
-string icon[] = {"*x*", " xx", "* *"};
+std::string icon[] = {"*x*", " xx", "* *"};
 
 int main() {
 
     int k;
-    cin>>k;
+    std::cin>>k;
 
     for(int i = 0; i < 3; i++){
         for(int reps = 0; reps < k; reps ++){
             for(int j = 0; j < 3; j++){
                 for(int r = 0; r < k; r++){
-                    cout<<icon[i][j];
+                    std::cout<<icon[i][j];
                 }
             }
-            cout<<endl;
+            std::cout<<std::endl;
         }
     }
 
