bef1497526f8e3124c072df5dca1ff3f4f08b25f
danh295 <hudanny295@gmail.com>
2021-11-12 17:33:15 -0500

    Create J2_SoundsFishy.cpp

---
+++ 2012/Junior/J2_SoundsFishy.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+
+    if(a < b && b < c && c < d)
+        cout<<"Fish Rising";
+    else if(a > b && b > c && c > d)
+        cout<<"Fish Diving";
+    else if(a == b && b == c && c == d)
+        cout<<"Fish Constant";
+    else
+        cout<<"No Fish";
+
+    return 0;
+}
