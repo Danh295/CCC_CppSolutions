9e4de946dae90f2c928b6639eceaf86d2a834869
danh295 <hudanny295@gmail.com>
2021-11-12 17:35:02 -0500

    Removed namespace std

--- 2012/Junior/J2_SoundsFishy.cpp
+++ 2012/Junior/J2_SoundsFishy.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
     int a, b, c, d;
-    cin>>a;
-    cin>>b;
-    cin>>c;
-    cin>>d;
+    std::cin>>a;
+    std::cin>>b;
+    std::cin>>c;
+    std::cin>>d;
 
     if(a < b && b < c && c < d)
-        cout<<"Fish Rising";
+        std::cout<<"Fish Rising";
     else if(a > b && b > c && c > d)
-        cout<<"Fish Diving";
+        std::cout<<"Fish Diving";
     else if(a == b && b == c && c == d)
-        cout<<"Fish Constant";
+        std::cout<<"Fish Constant";
     else
-        cout<<"No Fish";
+        std::cout<<"No Fish";
 
     return 0;
 }
