c66c47d694b97c86fa3d7fa51570d406c6d6dfe2
danh295 <hudanny295@gmail.com>
2021-11-14 11:20:15 -0500

    Updated formatting & added comments

--- 2012/Junior/J3_IconScaling.cpp
+++ 2012/Junior/J3_IconScaling.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <string>
 
+// GLOBAL INIT
 std::string icon[] = {"*x*", " xx", "* *"};
 
+// PROCESS
 void scaleIcon(int k)
 {
-    for(int i = 0; i < 3; i++){
-        for(int reps = 0; reps < k; reps ++){
-            for(int j = 0; j < 3; j++){
-                for(int r = 0; r < k; r++){
+    for(int i = 0; i < 3; i++)
+    {
+        for(int reps = 0; reps < k; reps ++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                for(int r = 0; r < k; r++)
+                {
                     std::cout<<icon[i][j];
                 }
             }
@@ -17,11 +23,15 @@ void scaleIcon(int k)
     }
 }
 
-int main() {
-
+int main() 
+{
+    // INITILIZATION
     int k;
+
+    // INPUT
     std::cin>>k;
 
+    // OUTPUT
     scaleIcon(k);
 
     return 0;
