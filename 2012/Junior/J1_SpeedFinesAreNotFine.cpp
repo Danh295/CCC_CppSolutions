a9961ad0098700ef4cefed66d67e0507a7b332f5
danh295 <hudanny295@gmail.com>
2021-11-16 22:43:24 -0500

    Removed problem description
    Problems can be found in newly added pdf file in the same folder

--- 2012/Junior/J1_SpeedFinesAreNotFine.cpp
+++ 2012/Junior/J1_SpeedFinesAreNotFine.cpp
@@ -1,23 +1,3 @@
-/*
-PROBLEM DESCRIPTION:
-Many communities now have radar signs that tell drivers what their speed is, in hopes that they will slow down.
-
-You will output a message for a radar sign. The message will display information to a driver based on his/her speed according to the following table:
-
-    km/h over the limit     fine
-        1 to 20             $100
-        21 to 30            $270
-        31 or above         $500
-
-Input Specification:
-The user will be prompted to enter 2 integers. First, the user will be prompted to enter the speed limit. Second, the user will be prompted to enter the 
-recorded speed of the car.
-
-Output Specification:
-If the driver is not speeding, the output should be: "Congratulations, you are within the speed limit!"
-If the driver is speeding, the output should be: "You are speeding and your fine is $'f'", where 'F' is the amount of the fine as described in the table above.
-*/
-
 #include <iostream>
 
 int main()
