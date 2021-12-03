d7a426b21c24ed97ff31ce24fe8384cdbe310397
danh295 <hudanny295@gmail.com>
2021-12-02 23:07:29 -0500

    Removed use of namespace std

--- 2020/Junior/J1_DogTreats.cpp
+++ 2020/Junior/J1_DogTreats.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
     int s;
     int m;
     int l;
-    cin>>s;
-    cin>>m;
-    cin>>l;
+    std::cin>>s;
+    std::cin>>m;
+    std::cin>>l;
 
     int score = s + 2*m + 3*l;
     if(score >= 10)
-        cout<<"happy";
+        std::cout<<"happy";
     else
-        cout<<"sad";
+        std::cout<<"sad";
 
     return 0;
 }
