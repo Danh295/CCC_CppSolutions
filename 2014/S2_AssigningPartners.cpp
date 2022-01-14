2fcdf637296c19d022cb220aebcc71c941f47092
danh295 <hudanny295@gmail.com>
2022-01-13 21:12:48 -0500

    Update S2_AssigningPartners.cpp
    Currently not getting full points, will fix soon

--- 2014/S2_AssigningPartners.cpp
+++ 2014/S2_AssigningPartners.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
@@ -8,9 +10,40 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    int n;
+    string names[2][30];
+    unordered_map<string, string> partners;
+
+    cin>>n;
     
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < n; j++)
+            cin>>names[i][j];
+    }
 
+    for(int i = 0; i < n; i++)
+    {
+        if(names[0][i] != names[1][i])
+            partners[names[0][i]] = names[1][i];
+    }
 
+    bool good = true;
+    for(auto i: partners)
+    {
+        if(i.first == i.second)
+            good = false;
+        else if(partners[i.second] != i.first)
+            good = false;
+        
+        if(!good)
+            break;
+    }
+
+    if(good)
+        cout<<"good";
+    else
+        cout<<"bad";
 
     return 0;
 }
