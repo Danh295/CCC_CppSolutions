35b6f9992d124b060356a88633bbdc59183d6a89
danh295 <hudanny295@gmail.com>
2022-02-05 12:44:48 -0500

    Update J1_WhatIsn,Daddy.cpp
    Included brute force solution and comments

--- 2010/J1_WhatIsn,Daddy.cpp
+++ 2010/J1_WhatIsn,Daddy.cpp
@@ -2,16 +2,27 @@
 
 using namespace std;
 
-int main() {
+int main() 
+{
+    // INITIALIZATION
 
+    // fast i/o
     ios::sync_with_stdio(0);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
     int n;
+
+    // INPUT
     cin>>n;
 
+    // PROCESS & OUTPUT
     cout<<(3 - abs(5 - n) / 2);
 
-    
+    // Brute force solution (slightly slower):
+    // if(n == 1 || n == 9 || n == 10)
+    //     cout<<1;
+    // else if(n == 2 || n == 3 || n == 7 || n == 8) 
+    //     cout<<2;
+    // else
+    //     cout<<3;
 }
