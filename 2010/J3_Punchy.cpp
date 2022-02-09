715e17b0da9494703d27ca634c5a0442c091266c
danh295 <hudanny295@gmail.com>
2022-02-08 23:52:07 -0500

    Create J3_Punchy.cpp

---
+++ 2010/J3_Punchy.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    string inst;
+
+    while()
+    {
+        
+    }
+
+    return 0;
+}
