47d73d754124f8d18a5ed2c7d5a99fd74a5b3b3b
danh295 <hudanny295@gmail.com>
2022-03-05 23:57:23 -0500

    Update J1_DogTreats.cpp

--- 2020/J1_DogTreats.cpp
+++ 2020/J1_DogTreats.cpp
@@ -2,13 +2,23 @@
 
 int main()
 {
+    // INITIALIZATION
+
+    // fast i/o
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
     int s;
     int m;
     int l;
+
+    // INPUT
     std::cin>>s;
     std::cin>>m;
     std::cin>>l;
 
+    // CONDITIONAL OUTPUT
     int score = s + 2*m + 3*l;
     if(score >= 10)
         std::cout<<"happy";
