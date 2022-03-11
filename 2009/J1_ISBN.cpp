5be82a8e35e4df09ac2d986d791bfe87a8ed6627
danh295 <hudanny295@gmail.com>
2022-03-11 09:47:04 -0500

    Update J1_ISBN.cpp

--- 2009/J1_ISBN.cpp
+++ 2009/J1_ISBN.cpp
@@ -3,12 +3,18 @@
 using namespace std;
 
 int main() {
+    // INTITIALIZATION
+    
+    // fast i/o
+    ios::sync_with_stdio(0);
+    // can't tie I/O stream since question asks for input prompt
 
     int sumNum = 1;
     int firstTen[] = {9, 7, 8, 0, 9, 2, 1, 4, 1, 8};
     int lastThree[3];
     int one_three_sum = 0;
 
+    // INPUT
     cout<<"Digit 11? ";
     cin>>lastThree[0];
     cout<<"Digit 12? ";
@@ -16,7 +22,9 @@ int main() {
     cout<<"Digit 13? ";
     cin>>lastThree[2];
 
-    for (int i = 0; i < 10; i++) {
+    // PROCESS
+
+    for (int i = 0; i < 10; i++) { // accumulate 1-3-sum of first 10 digits
         one_three_sum += firstTen[i] * sumNum;
         if (sumNum == 1) {
             sumNum = 3;
@@ -24,7 +32,7 @@ int main() {
             sumNum = 1;
         }
     }
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) { // add 1-3-sum of the last 3 digits
         one_three_sum += lastThree[i] * sumNum;
         if (sumNum == 1) {
             sumNum = 3;
@@ -33,6 +41,7 @@ int main() {
         }
     }
 
+    // OUTPUT
     cout<<one_three_sum;
 
     return 0;
