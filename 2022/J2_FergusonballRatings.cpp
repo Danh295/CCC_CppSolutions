a491f4090296f009fd8009bc9cdf03d69e791ea0
danh295 <hudanny295@gmail.com>
2022-04-02 17:25:19 -0400

    Update J2_FergusonballRatings.cpp

--- 2022/J2_FergusonballRatings.cpp
+++ 2022/J2_FergusonballRatings.cpp
@@ -3,14 +3,22 @@
 using namespace std;
 
 int main() {
-    
-    int numPlayers;
-    cin>>numPlayers;
 
+    // INITIALIZATION
+    // fast i/o
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int numPlayers;
     int count = 0;
     int pts, fouls;
     bool star = true;
+    
+    // INPUT
+    cin>>numPlayers;
 
+    // PROCESS
     for (int i = 0; i < numPlayers; i++) {
         cin>>pts>>fouls;
         if ((pts*5) - (fouls*3) > 40) {
@@ -20,6 +28,7 @@ int main() {
         }
     }
 
+    // OUTPUT
     cout<<count;
     if (star) cout<<"+";
 }
