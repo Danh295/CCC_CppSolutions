3ef18d5a476ebff0c95ca39c48d160b932c32a37
danh295 <hudanny295@gmail.com>
2022-04-28 23:21:39 -0400

    Update J3_Punchy.cpp

--- 2010/J3_Punchy.cpp
+++ 2010/J3_Punchy.cpp
@@ -1,4 +1,4 @@
-// NOT FULLY IMPLEMENTED
+// NOT FULLY IMPLEMENTED doesnt work rn
 #include <iostream>
 #include <string>
 
@@ -11,12 +11,52 @@ int main() {
 
     int a = 0;
     int b = 0;
+    int inst;
 
-    string inst;
+    string inp1;
+    string inp2;
 
-    while()
-    {
+    cin >> inst;
+    while(inst != 7) {
+        cin >> inp1 >> inp2;
 
+        if (inst == 1) {
+            if (inp1 == "A") {
+                a = stoi(inp2);
+            } else if (inp1 == "B") {
+                b = stoi(inp2);
+            }
+        } else if (inst == 2) {
+            if (inp1 == "A") {
+                cout << a;
+            } else if (inp2 == "B") {
+                cout << b;
+            }
+        } else if (inst == 3) {
+            if (inp1 == "A") {
+                a = a + b;
+            } else if (inp1 == "B") {
+                b = b + a;
+            }
+        } else if (inst == 4) {
+            if (inp1 == "A") {
+                a = a * b;
+            } else if (inp1 == "B") {
+                b = b * a;
+            }
+        } else if (inst == 5) {
+            if (inp1 == "A") {
+                a = a - b;
+            } else if (inp1 == "B") {
+                b = b - a;
+            }
+        } else if (inst == 6) {
+            if (inp1 == "A") {
+                a = a / b;
+            } else if (inp1 == "B") {
+                b = b / a;
+            }
+        }
     }
 
     return 0;
