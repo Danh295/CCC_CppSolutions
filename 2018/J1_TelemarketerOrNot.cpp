33dc060b53f057eb4725e76ce2812e2fdb7341c9
danh295 <hudanny295@gmail.com>
2022-07-25 18:20:22 -0400

    Minor commenting & formatting updates

--- 2018/J1_TelemarketerOrNot.cpp
+++ 2018/J1_TelemarketerOrNot.cpp
@@ -1,17 +1,3 @@
-/*
-PROBLEM DESCRIPTION:
-Here at the Concerned Citizens of Commerce (CCC), we have noted that telemarketers like to use seven-digit phone numbers 
-where the last four digits have three properties. Looking just at the last four digits, these properties are:
-
-- the first of these four digits is an  or ;
-- the last digit is an  or ;
-- the second and third digits are the same.
-
-For example, if the last four digits of the telephone number are "8229", "8338", or "9008", these are telemarketer numbers.
-
-Write a program to decide if a telephone number is a telemarketer number or not, based on the last four digits. If the number is not a telemarketer number, we should answer the phone, and otherwise, we should ignore it.
-*/
-
 #include <iostream>
 
 bool checkNum(int *nums)
