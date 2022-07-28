0678010e7521803d29976c1af3d53eb59d6cea74
danh295 <hudanny295@gmail.com>
2022-07-27 21:09:31 -0400

    Removed std namespace usage

--- 2018/S1_VoronoiVillages.cpp
+++ 2018/S1_VoronoiVillages.cpp
@@ -1,30 +1,28 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 int main() {
     
     // INIT & INPUT
 
     // fast i/o
-    ios::sync_with_stdio(0);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
 
     int n, pos;
     double size, ans;
 
-    cin >> n;
+    std::cin >> n;
     int v[n];
     double midPts[n-1];
 
     for (int i = 0; i < n; i++) { 
-        cin >> pos;
+        std::cin >> pos;
         v[i] = pos;
     }
 
     // PROCESS
-    sort(v, v + n);
+    std::sort(v, v + n);
     for (int i = 0; i < n - 1; i++) midPts[i] = (v[i] + v[i + 1]) / 2.0; // get midpoints between the villages
 
     ans = midPts[1] - midPts[0];
@@ -34,5 +32,5 @@ int main() {
     }
 
     // OUTPUT
-    cout << fixed << setprecision(1) << ans;
+    std::cout << std::fixed << std::setprecision(1) << ans;
 }
