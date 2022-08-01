eaf74beafd1f517003876abf67e9f16dd45c9486
danh295 <hudanny295@gmail.com>
2022-08-01 15:51:26 -0400

    Update J4_FromPrefixToPostfix.cpp

--- 2008/J4_FromPrefixToPostfix.cpp
+++ 2008/J4_FromPrefixToPostfix.cpp
@@ -2,18 +2,21 @@
 
 using namespace std;
 
+string str, op1, op2;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
     while (true) {
-        string str, op1, op2;
         getline(cin, str);
 
         stack<string> stk;
 
+        if (str == "0") break;
+
         for (int i = str.size() - 1; i >= 0; i--) {
+            if (str[i] == ' ') continue;
             if (str[i] == '+' || str[i] == '-') {
                 op1 = stk.top();
                 stk.pop();
