9620a96ee9c11552e74db0763144d3719e5fbfb6
danh295 <hudanny295@gmail.com>
2022-08-15 20:53:43 -0400

    Removed problem description
    in order to match general code format

--- 2017/J2_ShiftySum.cpp
+++ 2017/J2_ShiftySum.cpp
@@ -1,20 +1,3 @@
-/*
-PROBLEM DESCRIPTION:
-Suppose we have a number like 12. Let's define "shifting a number" to mean
-adding a zero at the end. For example, if we shift that number once, we get
-the number 120. If we shift the number again we get the number 1200. We
-can shift the number as many times as we want.
-
-In this problem you will be calculating a "shifty sum", which is the sum of a
-number and the numbers we get by shifting. Specifically, you will be given
-the starting number "N" and a non-negative integer "k". You must add together
-"N" and all the numbers you get by shifting a total of "k" times.
-
-For example, the shifty sum when "N" is 12 and "k" is 1 is: 12 + 120 = 132. As
-another example, the shifty sum when "N" is 12 and "k" is 3 is
-12 + 120 + 1200 + 12000 = 13332.
-*/
-
 #include <iostream>
 
 // MAIN PROCESS
