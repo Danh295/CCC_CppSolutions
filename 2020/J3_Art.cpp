85f7203cd33e67c10adc759e06ad8645cc95e888
danh295 <hudanny295@gmail.com>
2022-08-20 18:04:35 -0400

    Update J3_Art.cpp

--- 2020/J3_Art.cpp
+++ 2020/J3_Art.cpp
@@ -1,23 +1,32 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 int nDrops, x, y;
 int main() {
+    // fast io
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    // init vars
     char sep;
     int minX = 0, minY = 0, maxX = 0, maxY = 0;
 
-    cin >> nDrops;
-    cin >> x >> sep >> y;
+    // init input
+    std::cin >> nDrops;
+    std::cin >> x >> sep >> y;
 
+    // iterative input
     minX = x; minY = y; maxX = x; maxY = y;
     for (int i = 1; i < nDrops; i++) {
-        cin >> x >> sep >> y;
+        std::cin >> x >> sep >> y;
+
+        // conditionally update vars
         if (x < minX) minX = x;
         else if (x > maxX) maxX = x;
         if (y < minY) minY = y;
         else if (y > maxY) maxY = y;
     }
 
-    cout << minX - 1 << "," << minY - 1 << "\n" << maxX + 1 << "," << maxY + 1;
+    // output one greater the largest coords, one less the smallest coords
+    std::cout << minX - 1 << "," << minY - 1 << "\n" << maxX + 1 << "," << maxY + 1;
 }
