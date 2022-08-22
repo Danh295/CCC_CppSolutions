6cfb8ce4edbe739151800a3d8446c4883417f3b8
danh295 <hudanny295@gmail.com>
2022-08-22 15:06:46 -0400

    Removed use of namespace & updated formatting to match convention

--- 2021/S1_CrazyFency.cpp
+++ 2021/S1_CrazyFency.cpp
@@ -1,36 +1,29 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 int main() {
 
-    // INITIALIZATION
-
     // fast i/o 
-    ios::sync_with_stdio(0);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
 
+    // init vars
     int N;
-
-    // INPUT
-    cin>>N;
+    std::cin>>N;
 
     int heights[N+1];
     int widths[N];
 
     // iterative input
     for (int i = 0; i < (N+1); i++) {
-        cin>>heights[i];
+        std::cin >> heights[i];
     }
 
-    // PROCESS
     double area = 0;
     for (int i = 0; i < N; i++) {
-        cin>>widths[i];
+        std::cin >> widths[i];
         area += (heights[i] + heights[i+1]) / 2.0 * widths[i]; // calculate area of a trapezoid & increment to total area
     }
 
-    //  OUTPUT
-    cout<<setprecision(5)<<fixed<<area; // set pre-determined decimal places
+    std::cout << std::setprecision(5) << std::fixed << area; 
 }
