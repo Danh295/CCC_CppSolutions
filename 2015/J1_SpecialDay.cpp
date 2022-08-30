fdbd02bfcb2f68b0622bcfc4aba5104347d7f29e
danh295 <hudanny295@gmail.com>
2022-08-29 21:06:30 -0400

    Removed use of namespace

--- 2015/J1_SpecialDay.cpp
+++ 2015/J1_SpecialDay.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
     int month, day;
-    cin>>month;
-    cin>>day;
+    std::cin>>month;
+    std::cin>>day;
 
     if (month < 2)
-        cout<<"Before";
+        std::cout<<"Before";
     else if (month > 2)
     {
-        cout<<"After";
+        std::cout<<"After";
     }
     else 
     {
         if(day < 18)
-            cout<<"Before";
+            std::cout<<"Before";
         else if (day > 18)
-            cout<<"After";
+            std::cout<<"After";
         else
-            cout<<"Special";
+            std::cout<<"Special";
     }
 
     return 0;
