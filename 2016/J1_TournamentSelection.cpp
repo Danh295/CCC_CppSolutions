75d1d3088806ecb41961cb101322dbe3fb94fbc6
danh295 <hudanny295@gmail.com>
2022-09-03 23:08:13 -0400

    Removed unnecessary function, updated conventions to match

--- 2016/J1_TournamentSelection.cpp
+++ 2016/J1_TournamentSelection.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 
-int groupPlayer(int wins)
-{
-    if(wins >= 5)
-        return 1;
-    else if(wins >= 3)
-        return 2;
-    else if(wins >= 1)
-        return 3;
-    else
-        return -1;
-}
-
 int main()
 {
+    // fast i/o
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    // init
     char game;
     int wins = 0;
     
-    for(int i = 0; i < 6; i++)
-    {
+    for (int i = 0; i < 6; i++) {
         std::cin >> game;
-        if(game == 'W')
-            wins += 1;
+        if (game == 'W') wins += 1;
     }
 
-    std::cout << groupPlayer(wins);
+    // conditional output
+    if (wins >= 5) std::cout << 1;
+    else if (wins >= 3) std::cout << 2;
+    else if (wins >= 1) std::cout << 3;
+    else std::cout << -1;
     
     return 0;
 }
