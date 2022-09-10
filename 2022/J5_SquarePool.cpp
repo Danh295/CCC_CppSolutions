dc5cb67c16e7d10a93aa1ad138cc6a2f0cbd4601
danh295 <hudanny295@gmail.com>
2022-09-10 15:25:42 -0400

    Implemented someone else's algorithm
    Struggling to understand it though, so still working through it

--- 2022/J5_SquarePool.cpp
+++ 2022/J5_SquarePool.cpp
@@ -2,8 +2,11 @@
 
 using namespace std;
 
-vector<pair<int, int>> coords;
-int nTrees;
+const int MAX = 5e5 + 2;
+
+int N, T, ans = 0;
+vector<int> bounds = {0}; // keeps track of boundaries
+vector<pair<int, int>> trees;
 int main() 
 {
     // fast i/o
@@ -12,15 +15,36 @@ int main()
     cout.tie(0);
 
     // init
-    cin >> nTrees;
+    cin >> N >> T;
+
     int x, y;
-    pair<int, int> coord;
-    for (int i = 0; i < nTrees; i++) {
-        cin >> coord.first >> coord.second;
-        coords.push_back(coord);
+    for (int i = 0; i < T; i++) {
+        cin >> x >> y;
+        bounds.push_back(y);
+        trees.push_back({x, y});
     }
 
-    // recursively check for available squares
+    // sort order of coordiantes from closest to 1,1
+    sort(bounds.begin(), bounds.end());
+    sort(trees.begin(), trees.end());
 
+    int prev;
+    for (auto x : bounds) {
+        for (auto y : bounds) {
+            
+            if ( x >= y) continue;
+
+            prev = 0;
+            for (auto t : trees) {
+                if (t.second <= x || t.second >= y) continue;
+
+                ans = max(min(y - x - 1, t.first - prev - 1), ans);
+                prev = t.first;
+            }
+            ans = max(min(y - x - 1, N - prev), ans);
+        }
+    }
+
+    cout << ans;
     return 0;
 }
