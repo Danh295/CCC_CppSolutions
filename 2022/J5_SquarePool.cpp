2bfb5cc9222d55c90aa4594914504f604d880a6d
danh295 <hudanny295@gmail.com>
2022-09-09 22:01:48 -0400

    Update J5_SquarePool.cpp

--- 2022/J5_SquarePool.cpp
+++ 2022/J5_SquarePool.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+vector<pair<int, int>> coords;
+int nTrees;
 int main() 
 {
-    
+    // fast i/o
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // init
+    cin >> nTrees;
+    int x, y;
+    pair<int, int> coord;
+    for (int i = 0; i < nTrees; i++) {
+        cin >> coord.first >> coord.second;
+        coords.push_back(coord);
+    }
+
+    // recursively check for available squares
 
     return 0;
 }
