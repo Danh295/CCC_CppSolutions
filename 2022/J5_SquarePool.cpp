2a450295eebe44eba9a34bd9210a83d94aa60aa1
danh295 <hudanny295@gmail.com>
2022-09-14 00:00:06 -0400

    Rewrote program to better understand the algorithm

--- 2022/J5_SquarePool.cpp
+++ 2022/J5_SquarePool.cpp
@@ -2,11 +2,9 @@
 
 using namespace std;
 
-const int MAX = 5e5 + 2;
-
-int N, T, ans = 0;
-vector<int> bounds = {0}; // keeps track of boundaries
+vector<int> y_vals = {0};
 vector<pair<int, int>> trees;
+int N, T, ans = 0;
 int main() 
 {
     // fast i/o
@@ -15,42 +13,38 @@ int main()
     cout.tie(0);
 
     // init
-    cin >> N >> T;
-
     int x, y;
+    cin >> N >> T;
     for (int i = 0; i < T; i++) {
         cin >> x >> y;
-        bounds.push_back(y);
+        y_vals.push_back(y);
         trees.push_back({x, y});
     }
-    bounds.push_back(N + 1);
+    y_vals.push_back(N + 1);
 
-    // sort order of coordiantes from closest to 1,1
-    sort(bounds.begin(), bounds.end());
+    // sort values to help iteration begin from the smallest value
+    sort(y_vals.begin(), y_vals.end());
     sort(trees.begin(), trees.end());
 
     int prev;
-    for (auto x : bounds) { // iterate through the lengths in between the y values of the trees
-        for (auto y : bounds) { // x should track the first (smaller) y value, while y tracks the second (greater) y value
-            cout << "bound - x: " << x << "\t" << "bound - y: " << y << endl;
-            if ( x >= y) continue; // if it's the same y value or if x value bound is lower, skip to the next y value
+    for (int first : y_vals) { // iterate through the lengths in between the y values of the trees
+        for (int second : y_vals) { // x should track the first (smaller) y value, while y tracks the second (greater) y value
+            if (first >= second) continue; // if it's the same y value or if x value bound is lower, skip to the next y value
 
             prev = 0; // track the last valid x value
             for (auto t : trees) { // iterate through the tree coordinates
-                cout << "tree coord: " << t.first << ", " << t.second << endl;
-                if (t.second <= x || t.second >= y) continue; // if the tree's y value is less than or equal to the x bound, or if it's greater than or equal to the y bound, skip to the next tree coords (this means that there is a tree within the given bounds)
+                if (t.second <= first || t.second >= second) continue; // if the tree's y value is less than or equal to the x bound, or if it's greater than or equal to the y bound, skip to the next tree coords (this means that there is a tree within the given bounds)
 
-                cout << "maximum of: " << "(minimum of: " << y - x - 1 << ", " << t.first - prev - 1 << "), " << ans << endl;
-                ans = max(min(y - x - 1, t.first - prev - 1), ans); // y - x - 1 gives the valid horizontal side length between the trees, t.first - prev - 1 gives the valid vertical side length 
-                                                                    // between the trees. Take the minimum of the two values to get the side length of the valid square. Update the value of the 
-                                                                    // answer if the minimum side length is greater than the current answer
+                ans = max(min(second - first - 1, t.first - prev - 1), ans); // y - x - 1 gives the valid horizontal side length between the trees, t.first - prev - 1 gives the valid vertical side length 
+                                                                             // between the trees. Take the minimum of the two values to get the side length of the valid square. Update the value of the 
+                                                                             // answer if the minimum side length is greater than the current answer
                 prev = t.first; // update the last x value
             }
-            cout << "maximum of: " << "(minimum of: " << y - x - 1 << ", " << N - prev << "), " << ans << endl;
-            ans = max(min(y - x - 1, N - prev), ans); // same as above, except the formula changes to N - prev
+            ans = max(min(second - first - 1, N - prev), ans); // same as above, except the formula changes to N - prev
         }
     }
 
     cout << ans;
+
     return 0;
 }
