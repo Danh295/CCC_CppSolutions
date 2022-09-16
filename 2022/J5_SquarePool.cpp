cbed87ba2f7bb0e1d8879cb58ab51761e70f6097
danh295 <hudanny295@gmail.com>
2022-09-15 23:39:01 -0400

    Removed use of namespace

--- 2022/J5_SquarePool.cpp
+++ 2022/J5_SquarePool.cpp
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
-vector<int> y_vals = {0};
-vector<pair<int, int>> trees;
+std::vector<int> y_vals = {0};
+std::vector<std::pair<int, int>> trees;
 int N, T, ans = 0;
 int main() 
 {
     // fast i/o
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
 
     // init
     int x, y;
-    cin >> N >> T;
+    std::cin >> N >> T;
     for (int i = 0; i < T; i++) {
-        cin >> x >> y;
+        std::cin >> x >> y;
         y_vals.push_back(y);
         trees.push_back({x, y});
     }
@@ -35,16 +33,16 @@ int main()
             for (auto t : trees) { // iterate through the tree coordinates
                 if (t.second <= first || t.second >= second) continue; // if the tree's y value is less than or equal to the x bound, or if it's greater than or equal to the y bound, skip to the next tree coords (this means that there is a tree within the given bounds)
 
-                ans = max(min(second - first - 1, t.first - prev - 1), ans); // y - x - 1 gives the valid horizontal side length between the trees, t.first - prev - 1 gives the valid vertical side length 
+                ans = std::max(std::min(second - first - 1, t.first - prev - 1), ans); // y - x - 1 gives the valid horizontal side length between the trees, t.first - prev - 1 gives the valid vertical side length 
                                                                              // between the trees. Take the minimum of the two values to get the side length of the valid square. Update the value of the 
                                                                              // answer if the minimum side length is greater than the current answer
                 prev = t.first; // update the last x value
             }
-            ans = max(min(second - first - 1, N - prev), ans); // same as above, except the formula changes to N - prev
+            ans = std::max(std::min(second - first - 1, N - prev), ans); // same as above, except the formula changes to N - prev
         }
     }
 
-    cout << ans;
+    std::cout << ans;
 
     return 0;
 }
